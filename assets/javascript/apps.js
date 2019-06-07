c18010394277dc9d4ab2fee8bea8cc87a3275f89
main desktop <da.ga.mo.94@gmail.com>
2019-06-07 01:37:20 -0600

    attempted to add dynamic scrolling but not yet working. Change in the JS file only

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -52,7 +52,19 @@ function displayImages() {
 
 displayImages();
 
-// 
+// DYNAMIC SCROLLING
+
+$(document).ready(function(){
+
+    
+    var scrollLink = $(".scroll");
+    
+    scrollLink.on("click", function(event){
+        event.preventDefault();
+        $("body").animate({scrollTop: $(this.hash).offset().top},1000);
+        alert("should scroll");
+    });
+});
 
 
 
